9a3c0c4d78c783611a65c002aed3b85d01a6c35d
Johnny <designisalie@gmail.com>
2012-02-04 02:59:52 -0800

    Update reply.php

--- reply.php
+++ reply.php
@@ -12,8 +12,5 @@
 	$query = "INSERT INTO comments (name,comment,post_id,reply_to,date) values('".mysql_real_escape_string($name)."','".mysql_real_escape_string($com)."','".mysql_real_escape_string($post)."','".mysql_real_escape_string($reply)."','$date')";
 	$sql = mysql_query($query) or die ( "Query failed due to: ".mysql_error()); 
 	
-	if(!$sql)
-		echo "fuck";
-	
 	mysql_close($connection);
 ?>
